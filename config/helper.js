fa29e176a448b405fc09b8b4064834a798393131
ijoe7 <iyandaoluwasegun7@gmail.com>
2022-04-07 02:57:59 +0100

    endpoints update

--- config/helper.js
+++ config/helper.js
@@ -1,4 +1,6 @@
 const AppError = require("./appError");
+const _ = require("underscore");
+
 exports.validateParams = (req, next, request) => {
   request.map((item) => {
     if (!req.body[item]) return next(new AppError(`${item} is required`, 400));
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,7 @@ const user = require("../controllers/userController");
 
 router.post("/addUser", user.addUser);
 router.get("/getUser/:id", user.getUser);
+router.get("/getAllUsers", user.getAllUsers);
 router.put("/updateUser/:id", user.updateUser);
 router.delete("/deleteUser/:id", user.deleteUser);
 
